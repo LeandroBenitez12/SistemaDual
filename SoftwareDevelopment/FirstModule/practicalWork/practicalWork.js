0545b00d9b1bb42efd24c1868ca11ce25f19b4e0
LeandroBenitez12 <108737751+LeandroBenitez12@users.noreply.github.com>
2024-04-23 17:11:04 -0300

    exercise Two, i testing successfully!

--- SoftwareDevelopment/FirstModule/practicalWork/practicalWork.js
+++ SoftwareDevelopment/FirstModule/practicalWork/practicalWork.js
@@ -22,20 +22,38 @@ function wordSeparator(phrase, arg) {
 }
 
 function convertToTitle(phrase) {
+  var word = [];
   var title = "";
   const argument = " ";
   const wordList = wordSeparator(phrase, argument);
-  for (var i = 0; i < wordList.length; i++) {
-    wordList[i].toLowerCase();
-    console.log(title);
-    for (var j = 0; j < wordList[i].length; j++) {
-      wordList[i][0].toUpperCase();
-    }
-    title += wordList[i] + " ";
-  }
-  console.log(title);
+  for (var i = 0; i < wordList.length; i++) {}
   return title;
 }
 
 const phrase = "hello PEOPLE My name is LEANDRO ";
-convertToTitle(phrase);
+// convertToTitle(phrase);
+
+/*
+
+Concatenar elementos de un array con un separador personalizado:
+Crear una función que reciba un array de strings y un separador, y devuelva una sola cadena de
+texto que contenga todos los elementos del array concatenados con el separador especificado.
+● Ejemplo: Si la entrada es ["Hola", "Mundo", "JavaScript"] y el separador “-”, la
+salida debería ser "Hola-Mundo-JavaScript"*/
+
+function concatenateArray(array, separator) {
+  console.log(typeof array);
+  if (typeof array != "object") return "Ingrese un Array";
+  textString = "";
+  for (var i = 0; i < array.length; i++) {
+    if (i >= array.length - 1) {
+      textString += array[i];
+    } else {
+      textString += array[i] + separator;
+    }
+  }
+  return textString;
+}
+array = ["Hello", "World", "I", "am", "Leandro"];
+
+console.log(concatenateArray(array, "*"));
